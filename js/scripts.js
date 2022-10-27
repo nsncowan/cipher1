a0b047f714ebfff251d794f2b6ddca5e06971592
Noah Cowan <nsncowan@gmail.com>
2022-10-26 20:34:36 -0700

    make it work.
    Co-authored by: Carl Sustrich <carl.sustrich@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,14 @@
 const userInput = window.prompt('enter a sentence you want to encrypt.')
 
-function firstLast {
-  let letters = userInput.charAt(0) + userInput.charAt(userInput.length -1);
-  let capLetters = letters.toUpperCase();
+function firstLast(userInput) {
+  const letters = userInput.charAt(0) + userInput.charAt(userInput.length -1);
+  const capLetters = letters.toUpperCase();
   return capLetters;
 }
 
-function lastFirst {
-  let reversedLetters = capLetters.charAt(1) + capLetters.charAt(0);
+function lastFirst(capLetters) {
+  const reversedLetters = capLetters.charAt(1) + capLetters.charAt(0);
+  return reversedLetters;
 }
+
+window.alert(lastFirst(firstLast(userInput)));
